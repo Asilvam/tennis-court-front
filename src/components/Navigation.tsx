4d5bb45042447860d1fb6f404570fca424213c68
alejandrosilvamonsalve <alejmsilvam@falabella.cl>
2024-10-24 19:10:15 -0300

    improve code

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -50,7 +50,7 @@ const Navigation: React.FC = () => {
     const navItems = [
         { to: '/dashboard', label: 'Reserves Courts', show: tokenExists },
         { to: '/myhistory', label: 'My History', show: tokenExists },
-        { to: '/adminregister', label: 'Admin Reserves', show: tokenExists && isUserRoleAdmin },
+        { to: '/adminregister', label: 'Admin Users', show: tokenExists && isUserRoleAdmin },
     ];
 
     return (
